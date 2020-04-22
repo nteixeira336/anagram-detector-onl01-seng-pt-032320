93d43abf44abca3c634adee5c4c2b6cdb1fbc5d4
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-04-22 13:17:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram 
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(array_of_anagrams)
+    array_of_anagrams.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
+    end 
+  end 
+  
+end 
